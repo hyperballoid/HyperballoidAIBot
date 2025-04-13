8b1df3aa48b93a8c6c170e75b7e9b48414351ecc
hyperballoid <80431938+hyperballoid@users.noreply.github.com>
2025-04-13 13:17:05 +0300

    Update news.py

--- bot/news.py
+++ bot/news.py
@@ -1,1 +1,8 @@
+import requests
 
+def get_news(api_key):
+    try:
+        response = requests.get(f"https://newsapi.org/v2/everything?q=AI+art&apiKey={api_key}")
+        return response.json()["articles"][:3]
+    except:
+        return []
