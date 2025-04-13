976117e70f4d845b0f880a8e0ca98e54de3e32d5
hyperballoid <80431938+hyperballoid@users.noreply.github.com>
2025-04-13 10:58:46 +0300

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,7 @@ import requests
 import json
 from datetime import datetime
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"  # Вставиш на кроці 5
+BOT_TOKEN = "7884645795:AAHV9gm-XLivebS32XWFNp2fsvxGjsQ4HNc"  # Вставиш на кроці 5
 CHANNEL_ID = "@HyperballoidAIArt"
 
 def start(update, context):
